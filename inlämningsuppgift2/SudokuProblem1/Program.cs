b93af4187a5aa1ff46fac7bfe972e99a942fb950
Jonas Lindstrom <jonas.lindstrom@yh.nackademin.se>
2017-09-13 15:09:12 +0200

    Constructor ready and PrintBoard() ready

--- inlämningsuppgift2/SudokuProblem1/Program.cs
+++ inlämningsuppgift2/SudokuProblem1/Program.cs
@@ -10,9 +10,24 @@ namespace SudokuProblem1
     {
         static void Main(string[] args)
         {
+            Sudoku game = new Sudoku("003020600900305001001806400" +
+            "008102900700000008006708200" +
+            "002609500800203009005010300");
+            bool result = game.Solve();
 
+            if (result)
+            {
+                game.PrintBoard();
+            }
+            else
+            {
+                Console.WriteLine("Det misslyckades att lösa Sudokut");
+            }
+            Console.WriteLine();    
 
-
+            Sudoku game2 = new Sudoku("619030040270061008000047621486302079000014580031009060005720806320106057160400030");
+            game2.Solve();
+            game2.PrintBoard();
         }
     }
 }
--- inlämningsuppgift2/SudokuProblem1/Sudoku.cs
+++ inlämningsuppgift2/SudokuProblem1/Sudoku.cs
@@ -8,5 +8,53 @@ namespace SudokuProblem1
 {
     class Sudoku
     {
+        private int[,] sudokuCells;
+
+        public Sudoku(string sudokuProblem)
+        {
+            sudokuCells = new int[9, 9];
+            for (int i = 0; i < sudokuProblem.Length; i++)
+            {
+                int row = i / 9;
+                int column = i % 9;
+                this.sudokuCells[row, column] = int.Parse(sudokuProblem.Substring(i, 1));
+            }
+        }
+
+        internal bool Solve()   // TODO Implement this method
+        {
+            return true;
+        }
+
+        internal void PrintBoard()
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                if (row%3 == 0)
+                {
+                    for (int i = 0; i < 25; i++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.WriteLine();
+                }
+
+                for (int column = 0; column < 9; column++)
+                {
+                    if (column%3 == 0)
+                    {
+                        Console.Write("| ");
+                    }
+                    Console.Write(sudokuCells[row, column] + " ");   
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
     }
 }
