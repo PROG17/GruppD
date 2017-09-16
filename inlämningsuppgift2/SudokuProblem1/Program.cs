0471825d5addec8b4544ad26a8980092c0b323cb
Emma Karlsson <emma_karlsson_93@hotmail.com>
2017-09-16 15:56:49 +0200

    Lade till en meny för spelval, 1,2,3, samt min egna mall för problem 2.

--- inlämningsuppgift2/SudokuProblem1/Program.cs
+++ inlämningsuppgift2/SudokuProblem1/Program.cs
@@ -3,74 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SudokuProblem1
 {
     class Program
     {
+
+        [STAThread]
         static void Main(string[] args)
-        {            
-            Sudoku game = new Sudoku("003020600900305001001806400" +
-            "008102900700000008006708200" +
-            "002609500800203009005010300");
-            bool result = game.Solve();
+        {
+            //Man kan välja att göra ett val 1,2,3 där;
+            // 1 är inlämning 1, 2 inlämning 2 och 3dje är inlämning 3
 
-            if (result)
-            {
-                game.PrintBoard();
-            }
-            else
-            {
-                Console.WriteLine("Det gick inte att lösa Sudokut");
-            }
-            Console.WriteLine();    
-            
-            Sudoku game2 = new Sudoku("619030040270061008000047621486302079000014580031009060005720806320106057160400030");
-            game2.Solve();
-            game2.PrintBoard(); 
-            
-            
-            Sudoku game3 = new Sudoku("186403000034080000700050408" +
-            "609042013010060070250730609" +
-            "805010002000070380000208561");
-            game3.PrintBoard();
-            Console.WriteLine();
-                        
-            bool result3 = game3.Solve();
 
-            if (result3)
-            {
-                Console.WriteLine("Det gick bra att lösa Sudokut !");
-                game3.PrintBoard();
-            }
-            else
-            {              
-                Console.WriteLine("Det gick inte att lösa Sudokut !!");
-                game3.PrintBoard();
-            }
-            Console.WriteLine();    
-            
-            Sudoku game4 = new Sudoku("100040785040000019000001004" +
-            "004003800005402600008900500" +
-            "400300000520000030397020006");
-            game4.PrintBoard();
-            Console.WriteLine();
-                        
-            bool result4 = game4.Solve();
+            Console.WriteLine("Hej välj vilket spel du vill köra:");
+            Console.WriteLine("1) Sudoku lösare - Programmet löser sudoku åt dig\n2) Här väljer du svårighetsgrad och får ut sudoku du ska lösa\n" +
+               "3) -----");
 
-            if (result4)
+            while (true)
             {
-                Console.WriteLine("Det gick bra att lösa Sudokut !");
-                game4.PrintBoard();
+                int whatGame = int.Parse(Console.ReadLine());
+
+                if (whatGame == 1)
+                {
+                    Console.WriteLine("Hej och välkommen till Sudoku! Skriv in ditt sudoku problem: ");
+                    Console.WriteLine("Varje tom cell ska skrivas som 0");
+                    string input = Console.ReadLine();
+
+                    Sudoku game = new Sudoku(input);
+
+                    bool result = game.Solve();
+
+                    if (result)
+                    {
+                        Console.WriteLine("Det gick att lösa:");
+                        game.PrintBoard();
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Tyvärr. Det gick inte att lösa sudokut: ");
+                        game.PrintBoard(); //För att skriva ut snyggt även om det inte gick att lösa
+                    }
+
+                    Console.WriteLine();
+
+
+                    Console.WriteLine("Nu kommer du att kunna lösa ett sudoku beroende på svårighetsgrad!");
+                    Console.ReadLine();
+                }
+
+                if (whatGame == 2)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new SudokuProblem2());
+                }
+
+                if (whatGame == 3)
+                {
+                    //Kod för val nummer 3, uppgift tre's genväg ska kodas hit senare
+                }
+
+
+                else
+
+                    Console.WriteLine("Fel input");
             }
-            else
-            {              
-                Console.WriteLine("Det gick inte att lösa Sudokut !!");
-                game4.PrintBoard();
-            }
-            Console.WriteLine();       
-                       
-            Console.ReadKey(true);
         }
     }
 }
